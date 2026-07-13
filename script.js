aea91bea3821d63323348c0601bad02dc6789cad
leviledup <hhvhuj53@gmail.com>
2026-07-13 18:41:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,10 @@ imageLoader.addEventListener('change', (e) => {
     const reader = new FileReader();
     reader.onload = (event) => {
         const img = new Image();
-        img.onload = () => { ctx.drawImage(img, 0, 0, 1280, 720); };
+        img.onload = () => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        };
         img.src = event.target.result;
     };
     reader.readAsDataURL(e.target.files[0]);
@@ -22,10 +25,10 @@ function addText() {
     ctx.fillStyle = "white";
     ctx.strokeStyle = "#89CFF0";
     ctx.lineWidth = 15;
-    ctx.font = "bold 120px Inter";
+    ctx.font = "bold 120px Inter, sans-serif";
     ctx.textAlign = "center";
-    ctx.strokeText(text, 640, 360);
-    ctx.fillText(text, 640, 360);
+    ctx.strokeText(text, canvas.width / 2, canvas.height / 2);
+    ctx.fillText(text, canvas.width / 2, canvas.height / 2);
 }
 
 function downloadImage() {
