6039e9e8218c6d036ced9757cc5cbbbc2ca828f2
leviledup <hhvhuj53@gmail.com>
2026-07-15 03:52:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-// add hamburger menu here
+const menuBtn = document.getElementById('menuBtn');
+const closeBtn = document.getElementById('closeDrawer');
+const drawer = document.getElementById('toolDrawer');
+
+menuBtn.addEventListener('click', () => drawer.classList.add('active'));
+closeBtn.addEventListener('click', () => drawer.classList.remove('active'));
